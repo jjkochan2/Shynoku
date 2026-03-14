36e13016df317ba761c3ce57c3ca1e7f4d6209f6
jjkochan2 <jjkocahn2@gmail.com>
2026-03-14 14:21:33 -0500

    create LevelSelectScreen

---
+++ app/levelSelect/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function LevelSelectScreen() {
+    return (
+        <View>
+            <Text>Level Select Screen</Text>
+        </View>
+    );
+}
