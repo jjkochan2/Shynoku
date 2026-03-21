a3e4f140215d50efb41c4cfee102f6dd912ed0fd
jjkochan2 <jjkocahn2@gmail.com>
2026-03-21 14:29:56 -0500

    split level screen into title, board, and pieces

--- app/level/[id].tsx
+++ app/level/[id].tsx
@@ -1,19 +1,55 @@
 import { StyleSheet, Text, View } from "react-native"
 import { useLocalSearchParams } from "expo-router"
+import { colors } from "../../src/theme/colors"
 
 const styles = StyleSheet.create({
     levelScreen: {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-    }
+        backgroundColor: colors.background
+    },
+    title: {
+        flex: 1,
+        justifyContent: "center",
+        borderWidth: 2,
+        borderColor: "red"
+    },
+    titleText: {
+        color: "white",
+        fontSize: 24,
+        fontWeight: 600,
+    },
+    board: {
+        flex: 1,
+        justifyContent: "center",
+        borderWidth: 2,
+        borderColor: "yellow"
+    },
+    pieces: {
+        flex: 1,
+        justifyContent: "center",
+        borderWidth: 2,
+        borderColor: "green"
+    },
+
 })
 
 export default function LevelScreen() {
     const { id } = useLocalSearchParams();
     return (
         <View style={styles.levelScreen}>
-            <Text>Level {id} Screen</Text>
+            <View style={styles.title}>
+                <Text style={styles.titleText}>
+                    Level {id}
+                </Text>
+            </View>
+            <View style={styles.board}>
+                <Text>Board</Text>
+            </View>
+            <View style={styles.pieces}>
+                <Text>Pieces</Text>
+            </View>
         </View>
     );
 }
