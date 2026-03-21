f74f4897dbe5623706f7acefc72d126c1cd2804c
jjkochan2 <jjkocahn2@gmail.com>
2026-03-21 11:24:34 -0500

    add level1 button to level select screen

--- app/levelSelect/index.tsx
+++ app/levelSelect/index.tsx
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native"
+import { Pressable, StyleSheet, Text, View } from "react-native"
+import { useRouter } from "expo-router"
+
+const styles = StyleSheet.create({
+    levelSelectScreen: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#0077ffff",
+    },
+    level1Button: {
+        backgroundColor: "black",
+        padding: "3%",
+        borderRadius: 12,
+        aspectRatio: 1,
+        alignItems: "center"
+    },
+    level1ButtonText: {
+        color: "white",
+        fontWeight: "600",
+        fontSize: 24,
+
+    }
+})
 
 export default function LevelSelectScreen() {
+    const router = useRouter();
     return (
-        <View>
+        <View style={styles.levelSelectScreen}>
             <Text>Level Select Screen</Text>
+            <Pressable
+                style={styles.level1Button}
+                onPress={() => {router.navigate('/level1')}}
+            >
+                <Text style={styles.level1ButtonText}>
+                    1
+                </Text>
+            </Pressable>
         </View>
     );
 }
