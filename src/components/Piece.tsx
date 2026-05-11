6348ade830888b8885fc04900bc798b65c54c47c
jjkochan2 <jjkocahn2@gmail.com>
2026-05-10 20:34:04 -0500

    make pieces move onDrag

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -1,4 +1,11 @@
-import { FlatList, StyleSheet, View } from "react-native";
+import { useRef } from "react";
+import {
+	Animated,
+	FlatList,
+	PanResponder,
+	StyleSheet,
+	View,
+} from "react-native";
 
 import Tile, { TileProps } from "./Tile";
 
@@ -16,16 +23,47 @@ const styles = StyleSheet.create({
 });
 
 export default function Piece({ tiles, numColumns }: PieceProps) {
+	const pan = useRef(new Animated.ValueXY()).current;
+
+	const panResponder = useRef(
+		PanResponder.create({
+			onMoveShouldSetPanResponder: () => true,
+
+			onPanResponderMove: Animated.event(
+				[
+					null,
+					{
+						dx: pan.x,
+						dy: pan.y,
+					},
+				],
+				{ useNativeDriver: false },
+			),
+
+			onPanResponderRelease: () => {
+				pan.x.setValue(0);
+				pan.y.setValue(0);
+			},
+		}),
+	).current;
+
 	return (
-		<View style={styles.piece}>
-			<FlatList
-				data={tiles}
-				numColumns={numColumns}
-				scrollEnabled={false}
-				keyExtractor={(_, index) => index.toString()}
-				renderItem={({ item }) => <Tile color={item.color} />}
-				contentContainerStyle={{ flexGrow: 1 }}
-			></FlatList>
-		</View>
+		<Animated.View
+			{...panResponder.panHandlers}
+			style={{
+				transform: [{ translateX: pan.x }, { translateY: pan.y }],
+			}}
+		>
+			<View style={styles.piece}>
+				<FlatList
+					data={tiles}
+					numColumns={numColumns}
+					scrollEnabled={false}
+					keyExtractor={(_, index) => index.toString()}
+					renderItem={({ item }) => <Tile color={item.color} />}
+					contentContainerStyle={{ flexGrow: 1 }}
+				></FlatList>
+			</View>
+		</Animated.View>
 	);
 }
