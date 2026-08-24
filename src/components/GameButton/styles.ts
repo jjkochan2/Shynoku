eb3e3d2c40988bb744357e25a81eb8f1c3b9f9a8
jjkochan2 <jjkocahn2@gmail.com>
2026-08-24 11:09:08 -0500

    align text for gameButtons

--- src/components/GameButton/styles.ts
+++ src/components/GameButton/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
 	button: {
 		justifyContent: "center",
+		alignItems: "center",
 		backgroundColor: "black",
 		paddingHorizontal: 32,
 		paddingVertical: 8,
